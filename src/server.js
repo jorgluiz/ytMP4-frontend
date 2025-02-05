f16de5ea73e9b0eb7387bf8bcda6fe4f2552e78f
jorgluiz <jorgeluizjr20@gmail.com>
2025-02-05 19:44:56 -0300

    fix: improve loading visibility and update server log

--- src/server.js
+++ src/server.js
@@ -26,5 +26,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`server running port web ${PORT}`);
+  console.log(`server running port web http://localhost:${PORT}`);
 });
